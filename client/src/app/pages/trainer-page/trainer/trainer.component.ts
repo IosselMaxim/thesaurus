f205ae2d0b2fc08865ba2d21d05dde697dc3bf45
Maxim <iossel.maxim@gmail.com>
2020-07-20 22:58:15 +0300

    add content - trainer-page

--- client/src/app/pages/trainer-page/trainer/trainer.component.ts
+++ client/src/app/pages/trainer-page/trainer/trainer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {pluck} from 'rxjs/operators';
+import {ActivatedRoute} from '@angular/router';
+import {Trainer} from '../../../interfaces/trainer';
+import {TRAINERS} from '../../../constants/trainers';
 
 @Component({
   selector: 'app-trainer',
@@ -7,10 +11,15 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TrainerComponent implements OnInit {
+  trainer: Trainer;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(pluck('id'))
+      .subscribe(id => {
+        this.trainer = TRAINERS.find(t => t._id === id);
+      });
   }
 
 }
