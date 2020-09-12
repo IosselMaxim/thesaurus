9f8bc709b58111c7e5e0f834d1ddb8ca074152d3
Maxim <iossel.maxim@gmail.com>
2020-09-12 23:29:00 +0300

    ng update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,6 +7,11 @@ import {NavComponent} from './core/components/nav/nav.component';
 import {SharedModule} from './shared/shared.module';
 import {TitleService} from './services/title.service';
 import {FooterComponent} from './footer/footer.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,6 +23,11 @@ import {FooterComponent} from './footer/footer.component';
     SharedModule,
     BrowserModule,
     AppRoutingModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [TitleService],
   bootstrap: [AppComponent]
